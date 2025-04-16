07e4c4e100feeda708465df5a9f0d26bc3b54b8f
jerald-raphel <jjeraldraphel@gmail.com>
2025-04-16 16:11:24 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,19 +1,51 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const app = express();
+// const authRoutes = require('./routes/auth');
+// const productRoutes = require('./routes/products');
+// const smsRoutes = require('./routes/smsRoutes');
+// app.use(cors());
+// app.use(express.json());
+// app.use('/api', authRoutes);
+// app.use('/api', productRoutes);
+// app.use('/api',smsRoutes);
+// mongoose.connect('mongodb+srv://jerald-db:jerald07!@cluster0.ylfuz.mongodb.net/ecommerce', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => console.log('MongoDB connected'));
+
+// const PORT = 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+
+// Routes
 const authRoutes = require('./routes/auth');
 const productRoutes = require('./routes/products');
 const smsRoutes = require('./routes/smsRoutes');
+
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Route handlers
 app.use('/api', authRoutes);
 app.use('/api', productRoutes);
-app.use('/api',smsRoutes);
-mongoose.connect('mongodb+srv://jerald-db:jerald07!@cluster0.ylfuz.mongodb.net/ecommerce', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'));
+app.use('/api', smsRoutes);
 
+// MongoDB connection
+mongoose.connect('mongodb+srv://jerald-db:jerald07!@cluster0.ylfuz.mongodb.net/ecommerce?retryWrites=true&w=majority', {
+  tls: true // Use TLS explicitly instead of deprecated options
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// Start server
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
