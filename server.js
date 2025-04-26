d8785969d8b2037a004366b35187386e84aed786
jerald-raphel <jjeraldraphel@gmail.com>
2025-04-26 16:35:28 +0530

    Updated server.js with new changes

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const smsRoutes = require('./routes/smsRoutes');
 
 // ✅ CORS Configuration
 const allowedOrigins = [
-  'https://ecommerce-mu-red-58.vercel.app',
+  'https://ecommerce-ruby-psi.vercel.app/',
   'http://localhost:4000',
 ];
 
